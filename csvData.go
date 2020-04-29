1303e2654c3557eb3a4746ba6f4fff261d934bc8
John Odejar <John.Adelbert.Odejar@ibm.com@Johns-MacBook-Pro.local>
2020-04-29 01:51:07 -0700

    more tests

--- csvData.go
+++ csvData.go
@@ -29,47 +29,41 @@ Error Cases:
 - returned data is not in fname,lname,age format
 - returned data is empty
 
-
-Error Handling:
-- log errors & ignore
-
 Think About:
 - how to retrieve list of urls or files to read? --> file of urls
-- concurrency = how many files to do concurrently? --> find out how many files and divide by memory size?
 - how to store data and use to get stats? --> 2 maps = (age -> count, age -> name) / have one for each file and compile into overall 
-- how to aggregate concurrency results from each file? 
 - how to deal with median value in between two data points? --> check if total records is even or odd, if even get
+
+- concurrency = how many files to do concurrently? --> find out how many files and divide by memory size?
+- how to aggregate concurrency results from each file? 
 - keep track of urls you could not get data from
 **/
 
 //reads input file of urls to call to get csv files
-func readInputFile() ([]string, error){
+func readInputFile() []string {
 	//check for file argument
 	if len(os.Args) < 2 {
 		err := errors.New("Missing required argument! Please provide file containing list of urls!")
-        log.Println(err.Error())
-        return nil, err
+        panic(err)
     }
 
     //read file
     log.Println("Attempting to read file...")
     data, err := ioutil.ReadFile(os.Args[1])
     if err != nil {
-        log.Println("Error reading file:", err.Error())
-        return nil, err
+        panic(err)
     }
 
     //check if data exists
     if len(data) == 0 {
     	err := errors.New("File of urls does not contain any data.")
-        log.Println(err.Error())
-        return nil, err
+        panic(err)
     }
 
     //make into list to iterate later
     urlList := strings.Split(string(data), "\n")
-    log.Println("Successfully retrieved list of urls. Need to retrieve", len(urlList), "csv files.")
-    return urlList, nil
+    log.Println("Need to retrieve", len(urlList), "csv files.")
+    return urlList
 }
 
 //get data from urls in batches using concurrency
@@ -124,6 +118,8 @@ func retrieveCsvDataFromUrl(csvUrl string) (map[int]int, map[int]string, []int, 
 
 	//read csv
 	reader := csv.NewReader(resp.Body)
+	reader.TrimLeadingSpace = true
+	reader.FieldsPerRecord = 3
 	data, err := reader.ReadAll()
 	if err != nil {
 		return nil, nil, nil, err
@@ -139,18 +135,7 @@ func organizeData(data [][]string) (map[int]int, map[int]string, []int, error) {
 	ageToName := make(map[int]string, 0)
 	ageList := make([]int, 0)
 
-	if len(data[0]) != 3 {
-		err := errors.New("Csv file data is not formatted correctly.")
-		log.Println("Error organizing data from csv:", err.Error())
-		return nil, nil, nil, err
-	}
-
 	for i, row := range(data[1:]) {
-		if len(row) != 3 {
-			log.Println("Row", i+1, "is not valid format for data.")
-			continue
-		}
-
 		//parse data
 		fname := strings.Trim(row[0], " ")
 		lname := strings.Trim(row[1], " ")
@@ -199,7 +184,7 @@ func getAverageAge(countMap map[int]int) {
 		sum += k * v
 		totalCount += v
 	}
-	avg := sum / totalCount
+	avg := float64(sum) / float64(totalCount)
 	log.Println("The average age is:", avg)
 }
 
@@ -217,7 +202,7 @@ func getMedianAgeAndName(countMap map[int]int, nameMap map[int]string, ageList [
 	sort.Ints(keyList)
 	sort.Ints(ageList)
 	log.Println(keyList)
-	log.Println(ageList)
+	// log.Println(ageList)
 	log.Println(countMap)
 	//check if median age is value in dataset
 	if totalCount % 2 == 1 {
@@ -243,20 +228,29 @@ func getMedianAgeAndName(countMap map[int]int, nameMap map[int]string, ageList [
 		//median is in between two values of dataset and may not exist in dataset
 		curInd := 0
 		medIndLow := (totalCount / 2) - 1
+		medIndHigh := medIndLow + 1
 		medAgeLow := 0
 		medAgeHigh := 0
 
 		log.Println("Total Count:",totalCount)
 		log.Println("Median Index Low:",medIndLow)
+		log.Println("Median Index High:",medIndHigh)
 		for i, k := range(keyList) {
 			log.Println("Current Index:",curInd)
-			if curInd == medIndLow {
+			if curInd == medIndLow && curInd < medIndHigh {
+				log.Println("Option1")
 				medAgeLow = k
 				medAgeHigh = keyList[i+1]
 				break
-			} else if curInd > medIndLow {
+			} else if curInd > medIndLow && curInd == medIndHigh {
+				log.Println("Option2")
 				medAgeLow = keyList[i-1]
 				medAgeHigh = k
+				break
+			} else if curInd > medIndLow && curInd > medIndHigh {
+				log.Println("Option3")
+				medAgeLow = keyList[i-1]
+				medAgeHigh = keyList[i-1]
 				break
 			} else {
 				val := countMap[k]
@@ -265,9 +259,9 @@ func getMedianAgeAndName(countMap map[int]int, nameMap map[int]string, ageList [
 		}
 		log.Println(ageList[medIndLow])
 		log.Println("The median low is:", medAgeLow)
-		log.Println(ageList[medIndLow+1])
+		log.Println(ageList[medIndHigh])
 		log.Println("The median high is:", medAgeHigh)
-		log.Println((ageList[medIndLow] + ageList[medIndLow+1])/2.0)
+		log.Println((ageList[medIndLow] + ageList[medIndHigh])/2.0)
 		medAge := (medAgeLow + medAgeHigh) / 2.0
 		log.Println("The median age is:", medAge)
 		if  val, found := nameMap[medAge]; found {
@@ -278,19 +272,15 @@ func getMedianAgeAndName(countMap map[int]int, nameMap map[int]string, ageList [
 	}
 }
 
-func processData() {
-	urlList, rerr := readInputFile()
-	if rerr != nil {
-		panic(rerr)
-	}
-
+func processCsvData() {
+	urlList := readInputFile()
 	finalCountMap, finalNameMap, finalAgeList := getCsvInBatches(urlList)
 	calculateStats(finalCountMap, finalNameMap, finalAgeList)
 }
 
 func main() {
 	start := time.Now()
-	processData()
+	processCsvData()
 	totalTime := time.Now().Sub(start)
 	log.Println("Total Runtime: ", totalTime)
 }
